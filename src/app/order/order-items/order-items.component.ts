32b367cacb8e5d20f6986c86d8529ebf6e28b092
Filipe Souza <lipeapostolo@yahoo.com.br>
2019-01-21 23:29:58 -0200

    Finalizado componente de Itens da compra

--- src/app/order/order-items/order-items.component.ts
+++ src/app/order/order-items/order-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order-items',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderItemsComponent implements OnInit {
 
+  @Input() items: CartItem[];
+
+  @Output() increaseQty = new EventEmitter<CartItem>();
+  @Output() decreaseQty = new EventEmitter<CartItem>();
+  @Output() remove = new EventEmitter<CartItem>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  emitIncreaseQty(item: CartItem){
+    this.increaseQty.emit(item);
+  }
+
+  emitDecreaseQty(item: CartItem){
+    this.decreaseQty.emit(item);
+  }
+
+  emitRemove(item: CartItem){
+    this.remove.emit(item);
+  }
 }
